50fb72a2773cd5042b8246becb6e618adf25ab69
GasserJC <57463922+GasserJC@users.noreply.github.com>
2020-01-23 18:38:06 -0600

    Create NameGuess.py

---
+++ NameGuess.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+try:
+  # Use the %tensorflow_version magic if in colab.
+  %tensorflow_version 2.x
+except Exception:
+  pass
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+import pandas as pd 
+
+data = pd.read_csv("SSA_Names_DB.csv")
+names = data.Name
+gender = data.Gender
+
+for i in range(0,len(names)-1):
+  names[i] = '{0:0<9}'.format(names[i])
+  if(len(names[i]) > 10):
+    names[i] = names[i][:9]
