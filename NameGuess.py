84213d7d42651b6663c507f073bcc9353df30550
Riley <rileyredfern@gmail.com>
2020-01-25 10:03:42 -0600

    Update NameGuess.py
    Jacob's code to convert the string names to integers and the genders to a zero or one in order for it to work properly.

--- NameGuess.py
+++ NameGuess.py
@@ -18,7 +18,22 @@ data = pd.read_csv("SSA_Names_DB.csv")
 names = data.Name
 gender = data.Gender
 
-for i in range(0,len(names)-1):
-  names[i] = '{0:0<9}'.format(names[i])
-  if(len(names[i]) > 10):
-    names[i] = names[i][:9]
+# converts the string names to integers
+num_names = [[0 for i in range(10)] for j in range(len(data))]  # j is nbr of names, i is length
+
+for i in range(0,len(names)): # makes all strings same length
+    names[i] = '{0:0<9}'.format(names[i])
+    if len(names[i]) > 10:
+        names[i] = names[i][:9]
+
+# makes all strings a list of ascii values and inputs to a new 2d array
+for i in range(0,len(names)):
+    for j in range(0, 9):
+        num_names[i][j] = ord(names[i][j])
+
+# converts the genders to integers
+for i in range(len(gender)):
+    if i == 'M':
+        i = 0
+    else:
+        i = 1
